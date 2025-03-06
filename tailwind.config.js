4d8361ac9a39bde3aa5fcaa8a76e5b713c94f16c
masood <masoodacheampong@gmail.com>
2025-03-06 08:05:06 +0000

    added changes to tailwindcss.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
